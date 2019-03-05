afd07b2bd9595a77aca1239f1ecb3ea8e5cd0d22
josephkem <josephkemoy@gmail.com>
2019-03-05 13:42:51 -0500

    Added a use method to allow the server to access the indx route

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+//Routes
+app.use ('/', require ('./routes/index'));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`Server started on port ${port}`));
