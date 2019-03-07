b2191cbfb32a6e774f19d653b925fd871c00db06
josephkem <josephkemoy@gmail.com>
2019-03-07 11:25:28 -0500

    Added a validation to check if a user already exists and encrypted the user password using bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+
+//User model
+const User = require('../models/User');
 
 //Login page
 router. get('/login', (req,res) => res.render('login'));
@@ -37,7 +42,43 @@ router.post('/register', (req,res)=> {
         });
 
     } else {
-        res.send('pass');
+        //Vallidation passed
+        User.findOne({ email: email })
+            .then ( user => {
+                if(user) {
+                    //User exists
+                    errors.push( {msg: 'Email is already registered'} )
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+
+                } else {
+                    const newUser = User ({
+                        name,
+                        email,
+                        password
+                    });
+
+                   //Hash password
+                   bcrypt.genSalt(10, (err, salt) =>
+                        bcrypt.hash(newUser.password, salt, (err,hash) => {
+                            if(err) throw err;
+                            //Set password to hashed
+                            newUser.password = hash;
+                            //Save user
+                            newUser.save()
+                                .then(user => {
+                                    res.redirect('/login');
+                                })
+                                .catch(err => console.log(err));
+
+                   }))
+                }
+            });
     }
 
 });
