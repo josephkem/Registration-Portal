b5e408a42796a36f38a3cc1e2c5e0c1edd256592
josephkem <josephkemoy@gmail.com>
2019-03-12 08:56:20 -0400

    Added flash and session middleware for flash messages

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 
 const app = express();
@@ -24,6 +26,16 @@ app.set('view engine', 'ejs');
 
 //Bodyparser
 app.use(express.urlencoded ( { extended: false }));
+
+//Express Session
+app.use(session ({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//Connect flash
+app.use(flash());
 
 //Routes
 app.use ('/', require ('./routes/index'));
