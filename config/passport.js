8fddbe0ac406976a166a7ec42be17a2ab34e8c84
josephkem <josephkemoy@gmail.com>
2019-03-12 10:37:03 -0400

    Added use method to match user

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,21 @@
 const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+//Load user model
+const User = require('../models/User');
+
+module.exports = (passport) {
+    passport.use (
+        new LocalStrategy ({ usernameField: 'email' }, (email, password, done) => {
+            //Match user
+            User.findOne({ email: email})
+            .then(user => {
+                if(!user) {
+                    return done(null, false, {message: 'That email is not registered'});
+                }
+            })
+            .catch(err => console.log(err) )
+        })
+    );
+}
