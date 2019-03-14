7d49e5b914a42dcd7e2027ee3dc8786b7ad20b2d
josephkem <josephkemoy@gmail.com>
2019-03-14 15:11:06 -0400

    Added an authentication to prevents users from accessing a page before logging in

--- routes/users.js
+++ routes/users.js
@@ -94,4 +94,12 @@ router.post('/login', (req, res, next) => {
     }) (req, res, next);
 });
 
+//Logout handle
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+
+})
+
 module.exports = router;
