cf17a5d4a4efb0599aa0519ea06e56be2b23bab3
josephkem <josephkemoy@gmail.com>
2019-03-14 13:37:09 -0400

    Added a login handle on the users file to redirect the page accordingly

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 
 //User model
@@ -82,6 +83,15 @@ router.post('/register', (req,res)=> {
             });
     }
 
+});
+
+//Login handle
+router.post('/login', (req, res) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    }) (req, res, next);
 });
 
 module.exports = router;
