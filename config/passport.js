f4376d2308f9bfa1721c1bab134f8fff6e52114d
josephkem <josephkemoy@gmail.com>
2019-03-14 12:59:50 -0400

    Added a passport method to serialize/deserilize users

--- config/passport.js
+++ config/passport.js
@@ -31,5 +31,13 @@ module.exports = (passport) => {
         })
     );
 
-    
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+          done(err, user);
+        });
+      });
 }
